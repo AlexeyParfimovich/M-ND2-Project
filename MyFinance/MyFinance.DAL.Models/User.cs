37cc91224e4c2ce5a05c324cdd8152ad69958c79
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2021-11-22 17:28:03 +0300

    DAL. Add User model and entity type configuration

---
+++ MyFinance/MyFinance.DAL.Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyFinance.DAL.Models
+{
+    public class User
+    {
+        public ulong Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
---
+++ MyFinance/MyFinance.DAL/TypeConfigurations/UserTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyFinance.DAL.Models;
+
+namespace MyFinance.DAL.TypeConfigurations
+{
+    internal class UserTypeConfiguration : IEntityTypeConfiguration<User>
+    {
+        private readonly string _tableName = "Users";
+
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(_tableName).HasKey(p => p.Id);
+
+            builder.Property(p => p.FirstName).HasMaxLength(32).IsRequired();
+
+            builder.Property(p => p.LastName).HasMaxLength(32);
+
+            builder.Property(p => p.Email).HasMaxLength(64).IsRequired();
+
+            builder.Property(p => p.Phone).HasMaxLength(32);
+
+            builder.Property(p => p.Login).HasMaxLength(32).IsRequired();
+
+            builder.Property(p => p.Password).HasMaxLength(32).IsRequired();
+
+        }
+    }
+}
