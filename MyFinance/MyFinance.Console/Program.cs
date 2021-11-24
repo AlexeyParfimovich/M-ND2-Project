983a0f448a4fc9b0d842b306810baed3b2db0e12
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2021-11-24 13:22:46 +0300

    DAL. Add console app to test DB access

---
+++ MyFinance/MyFinance.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFinance.DAL;
+using System;
+using System.Text.Json;
+
+namespace MyFinance.Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Test User table access");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            UnitOfWork db = new(new AppDbContext(optionsBuilder.Options));
+
+            var users = db.Users.GetAll().Result;
+
+            System.Console.WriteLine("Request all registered Users:");
+            System.Console.WriteLine($"\n{System.Text.Json.JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true })}");
+        }
+    }
+}
