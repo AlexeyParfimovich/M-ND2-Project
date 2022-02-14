2159e37f5d603cd03fbe4edf27836f612bde4d4a
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2022-02-14 10:35:15 +0300

    API. Refactoring UserHandlerMiddleware to implement user data caching

--- MyFinance/MyFinance.API/Infrastructure/UserHandlerMiddleware.cs
+++ MyFinance/MyFinance.API/Infrastructure/UserHandlerMiddleware.cs
@@ -1,15 +1,12 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using MyFinance.BLL.Common.Exceptions;
 using MyFinance.BLL.Common.Infrastructure;
-using MyFinance.BLL.Common.Interfaces;
 using MyFinance.BLL.Users.Dto;
 using MyFinance.DAL;
 using MyFinance.DAL.Entities;
 using System;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -19,10 +16,10 @@ namespace MyFinance.API.Infrastructure
     public class UserHandlerMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly ILogger<UserHandlerMiddleware> _logger; 
+        private readonly ILogger<UserHandlerMiddleware> _logger;
 
         public UserHandlerMiddleware(
-            RequestDelegate next, 
+            RequestDelegate next,
             ILogger<UserHandlerMiddleware> logger)
         {
             _next = next;
@@ -31,8 +28,6 @@ namespace MyFinance.API.Infrastructure
 
         public async Task Invoke(HttpContext context, IFinanceDbContext database)
         {
-            //var userClaims = context.User.Claims;
-
             if (context.User.Identity.IsAuthenticated)
             {
                 var dto = new FetchUserDto()
@@ -41,43 +36,50 @@ namespace MyFinance.API.Infrastructure
                     Email = context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email).Value
                 };
 
-                var entity = await database.Context.Users.FirstOrDefaultAsync(x => x.UserName == dto.UserName && x.Email == dto.Email);
+                var userId = await GetUser(database, dto);
 
-                if (entity is null)
+                if (userId == Guid.Empty)
                 {
-                    using var transaction = await database.Context.Database.BeginTransactionAsync();
-
-                    try
-                    {
-                        var entry = await database.Context.Users.AddAsync(ContractsMapper.Map<FetchUserDto, UserEntity>(dto));
-
-                        await database.Context.SaveChangesAsync();
-
-                        await transaction.CommitAsync();
-
-                        context.Items["UserId"] = entry.Entity.Id;
-
-                        _logger.LogInformation($"New user created: {entry.Entity.UserName}, {entry.Entity.Email}");
-                    }
-                    catch (Exception ex)
-                    {
-                        await transaction.RollbackAsync();
-
-                        throw new DataSaveChangesException($"Error saving new User", ex);
-                    }
+                    context.Items["UserId"] = await AddUser(database, dto);
+                    _logger.LogInformation($"New user created: {dto.UserName}, {dto.Email}");
                 }
                 else
                 {
-                    context.Items["UserId"] = entity.Id;
+                    context.Items["UserId"] = userId;
                 }
+            }
 
-            }
-            else
-            {
-                //context.Items["UserId"] = new Guid();
-            }
-            
             await _next(context);
+        }
+
+        private static async Task<Guid> AddUser(IFinanceDbContext database, FetchUserDto user)
+        {
+            using var transaction = await database.Context.Database.BeginTransactionAsync();
+
+            try
+            {
+                var entry = await database.Context.Users.AddAsync(ContractsMapper.Map<FetchUserDto, UserEntity>(user));
+
+                await database.Context.SaveChangesAsync();
+
+                await transaction.CommitAsync();
+
+                return entry.Entity.Id;
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+
+                throw new DataSaveChangesException($"Error adding new User", ex);
+            }
+        }
+
+        private static async Task<Guid> GetUser(IFinanceDbContext database, FetchUserDto user)
+        {
+            var entity = await database.Context.Users
+                .FirstOrDefaultAsync(x => x.UserName == user.UserName && x.Email == user.Email);
+
+            return entity is null ? Guid.Empty : entity.Id;
         }
     }
 }
