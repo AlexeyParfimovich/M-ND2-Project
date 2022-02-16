ae21e68bc7c37220d60c2db747f6e786ea2b3237
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2022-02-16 16:49:07 +0300

    RabbitMQ. Implement pattern "publish/subscribe"

--- MyFinance/MyFinance.RabbitMQ.SendLog/SendLog.cs
+++ MyFinance/MyFinance.RabbitMQ.SendLog/SendLog.cs
@@ -2,33 +2,22 @@
 using System.Text;
 using RabbitMQ.Client;
 
-namespace MyFinance.RabbitMQ.Send
+namespace MyFinance.RabbitMQ.SendLog
 {
-    class Send
+    class SendLog
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
 
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "tack_queue",
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
+				channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
 
                 var message = GetMessage(args);
                 var body = Encoding.UTF8.GetBytes(message);
-
-                var properties = channel.CreateBasicProperties();
-                properties.Persistent = true;
-
-                channel.BasicPublish(exchange: "",
-                    routingKey: "tack_queue",
-                    basicProperties: properties, //null,
-                    body: body);
+                channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
                 
                 Console.WriteLine(" [x] Sent {0}", message);
             }
@@ -39,7 +28,7 @@ namespace MyFinance.RabbitMQ.Send
 
         private static string GetMessage(string[] args)
         {
-            return $"{DateTime.Now}|INFO|{string.Join(" ", args)}";
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
         }
     }
 }
