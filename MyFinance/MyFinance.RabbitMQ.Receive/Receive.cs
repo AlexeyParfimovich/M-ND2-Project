85233624f34b368d36c84c16c7ced0eda58f72d2
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2022-02-17 13:02:06 +0300

    RabbitMQ. Implement pattern "request/reply"

--- MyFinance/MyFinance.RabbitMQ.Receive/Receive.cs
+++ MyFinance/MyFinance.RabbitMQ.Receive/Receive.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Text;
-using System.Threading;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
@@ -8,14 +7,14 @@ namespace MyFinance.RabbitMQ.Receive
 {
     class Receive
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
                 channel.QueueDeclare(queue: "tack_queue", // Just change the queue name to redefine it with new parameters
-                                     durable: true, // Make the queue durable - prevent it from deleting when the Server quits or crashes
+                                     durable: false, // Make the queue durable - prevent it from deleting when the Server quits or crashes
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
@@ -23,31 +22,53 @@ namespace MyFinance.RabbitMQ.Receive
                 // Don't dispatch a new message until the previous one has processed and acknowledged
                 channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
 
-                Console.WriteLine(" [*] Waiting for messages.");
-
                 var consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queue: "tack_queue", autoAck: false, consumer: consumer);
+
                 consumer.Received += (model, args) =>
                 {
-                    var body = args.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] Received {0}", message);
-                    
-                    // Fake task proceed simulation
-                    int dots = message.Split('.').Length - 1;
-                    Thread.Sleep(dots * 1000);
-                    Console.WriteLine(" [x] Tack done");
+                    var response = string.Empty;
 
-                    // Manual acknowledge delivered message(s)
-                    channel.BasicAck(deliveryTag: args.DeliveryTag, multiple: false);
+                    var body = args.Body.ToArray();
+                    var props = args.BasicProperties;
+
+                    var replyProps = channel.CreateBasicProperties();
+                    replyProps.CorrelationId = props.CorrelationId;
+
+                    try
+                    {
+                        var message = Encoding.UTF8.GetString(body);
+                        int n = int.Parse(message);
+                        Console.WriteLine(" [.] fib({0})", message);
+                        response = Fib(n).ToString();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(" [.] " + e.Message);
+                        response = "";
+                    }
+                    finally
+                    {
+                        var responseBytes = Encoding.UTF8.GetBytes(response);
+                        channel.BasicPublish(exchange: "", routingKey: props.ReplyTo, basicProperties: replyProps, body: responseBytes);
+                        channel.BasicAck(deliveryTag: args.DeliveryTag, multiple: false);
+                    }
                 };
 
-                channel.BasicConsume(queue: "tack_queue", 
-                    autoAck: false, //true
-                    consumer: consumer);
-
+                Console.WriteLine(" [*] Awaiting RPC requests");
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
             }
+        }
+
+        private static int Fib(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return Fib(n - 1) + Fib(n - 2);
         }
     }
 }
--- MyFinance/MyFinance.RabbitMQ.Send/Send.cs
+++ MyFinance/MyFinance.RabbitMQ.Send/Send.cs
@@ -1,45 +1,73 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Concurrent;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace MyFinance.RabbitMQ.Send
 {
     class Send
     {
-        static void Main(string[] args)
+        static void Main()
+        {
+            var rpcClient = new RpcClient();
+
+            Console.WriteLine(" [x] Requesting fib(30)");
+            var response = rpcClient.Call("30");
+
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
+        }
+    }
+
+
+    public class RpcClient
+    {
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly BlockingCollection<string> respQueue = new();
+        private readonly IBasicProperties props;
+
+        public RpcClient()
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
 
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            replyQueueName = channel.QueueDeclare().QueueName;
+            consumer = new EventingBasicConsumer(channel);
+
+            props = channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
+
+            consumer.Received += (model, args) =>
             {
-                channel.QueueDeclare(queue: "tack_queue",
-                    durable: true,
-                    exclusive: false,
-                    autoDelete: false,
-                    arguments: null);
+                var body = args.Body.ToArray();
+                var response = Encoding.UTF8.GetString(body);
+                if (args.BasicProperties.CorrelationId == correlationId)
+                {
+                    respQueue.Add(response);
+                }
+            };
 
-                var message = GetMessage(args);
-                var body = Encoding.UTF8.GetBytes(message);
-
-                var properties = channel.CreateBasicProperties();
-                properties.Persistent = true;
-
-                channel.BasicPublish(exchange: "",
-                    routingKey: "tack_queue",
-                    basicProperties: properties, //null,
-                    body: body);
-                
-                Console.WriteLine(" [x] Sent {0}", message);
-            }
-
-            Console.WriteLine(" Press [enter] to exit.");
-            Console.ReadLine();
+            channel.BasicConsume(consumer: consumer, queue: replyQueueName, autoAck: true);
         }
 
-        private static string GetMessage(string[] args)
+        public string Call(string message)
         {
-            return $"{DateTime.Now}|INFO|{string.Join(" ", args)}";
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "", routingKey: "rpc_queue", basicProperties: props, body: messageBytes);
+
+            return respQueue.Take();
+        }
+
+        public void Close()
+        {
+            connection.Close();
         }
     }
 }
